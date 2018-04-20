acf0ece5891479320bd5bb791b5e0b20ae05f246
清水幸佑 <shimizukousuke@ip-172-17-1-55.ap-northeast-1.compute.internal>
2018-04-20 10:47:48 +0900

    fix : Specify the port

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -21,10 +22,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ =
 			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "Application Address")
+	flag.Parse() // Interpret the flag
+
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
@@ -32,7 +36,8 @@ func main() {
 	//chatroom run!
 	go r.run()
 	// WebServer start
-	if err := http.ListenAndServe("172.17.1.17:8080", nil); err != nil {
+	log.Println("WebServer start. Port:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:err")
 	}
 }

ca79ed255b3bd0848d98998cd72277cd3a08fd9f
thimi0412 <thimi0412@icloud.com>
2018-08-22 23:07:28 +0900

    fix
    警告を対処

--- chat/avatar.go
+++ chat/avatar.go
@@ -5,16 +5,20 @@ import "errors"
 // ErrNoAvatarURL is Error when an instance of Avatar can not return avatar's URL
 var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できません")
 
+// Avatar is hold AvatarURL
 type Avatar interface {
 	// GetAvatarURL is avataer's URL
 	GetAvatarURL(c *client) (string, error)
 }
 
+// AuthAvatar empty struct
 type AuthAvatar struct{}
 
+// UseAuthAvatar is user auth
 var UseAuthAvatar AuthAvatar
 
-func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+// GetAvatarURL is method which return avatarURL
+func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
 	if url, ok := c.userData["avatar_url"]; ok {
 		if urlStr, ok := url.(string); ok {
 			return urlStr, nil

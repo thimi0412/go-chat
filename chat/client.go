006e188114c21c6b09db688634792021b4bf12d3
thimi0412 <thimi0412@icloud.com>
2018-08-22 15:17:28 +0900

    add
    ユーザ名が表示されるようにした

--- chat/client.go
+++ chat/client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -9,14 +11,19 @@ type client struct {
 	// socket is WebSocket for client
 	socket *websocket.Conn
 	// send is channel which messega sent
-	send chan []byte
+	send chan *message
 	// room is chatroom in which client is joining
 	room *room
+	// userDataはユーザーに関する情報を保持します
+	userData map[string]interface{}
 }
 
 func (c *client) read() {
 	for {
-		if _, msg, err := c.socket.ReadMessage(); err == nil {
+		msg := new(message)
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
 			c.room.forward <- msg
 		} else {
 			break
@@ -27,7 +34,7 @@ func (c *client) read() {
 
 func (c *client) write() {
 	for msg := range c.send {
-		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+		if err := c.socket.WriteJSON(msg); err != nil {
 			break
 		}
 	}
--- chat/room.go
+++ chat/room.go
@@ -4,13 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/stretchr/objx"
+
 	"github.com/gorilla/websocket"
 	"github.com/thimi0412/go-chat/trace"
 )
 
 type room struct {
 	// forward is channel which hold messege for transfer client
-	forward chan []byte
+	forward chan *message
 	// join is client who about to join a chatroom
 	join chan *client
 	// leave is client who about to leave a chatroom
@@ -24,7 +26,7 @@ type room struct {
 // newRoom creat new chatroom
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -45,7 +47,7 @@ func (r *room) run() {
 			close(client.send)
 			r.tracer.Trace("Leave client")
 		case msg := <-r.forward:
-			r.tracer.Trace("Reception　message : ", string(msg))
+			r.tracer.Trace("Reception　message : ", msg.Message)
 			// send message for all client
 			for client := range r.clients {
 				select {
@@ -79,10 +81,17 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("SeverHTTP", err)
 		return
 	}
+
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatalln("クッキーの取得に失敗しました:", err)
+		return
+	}
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
